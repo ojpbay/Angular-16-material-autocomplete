931ad79627875e2d40b4d8f4e4023f56d8103c71
ojpbay <1391064+ojpbay@users.noreply.github.com>
2023-06-02 17:38:19 +0100

    add id to model

--- src/example/autocomplete-overview-example.ts
+++ src/example/autocomplete-overview-example.ts
@@ -1,14 +1,15 @@
-import {Component} from '@angular/core';
-import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {NgFor, AsyncPipe} from '@angular/common';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NgFor, AsyncPipe } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 export interface State {
+  id: number;
   flag: string;
   name: string;
   population: string;
@@ -39,24 +40,28 @@ export class AutocompleteOverviewExample {
 
   states: State[] = [
     {
+      id: 1,
       name: 'Arkansas',
       population: '2.978M',
       // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
       flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg',
     },
     {
+      id: 2,
       name: 'California',
       population: '39.14M',
       // https://commons.wikimedia.org/wiki/File:Flag_of_California.svg
       flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg',
     },
     {
+      id: 3,
       name: 'Florida',
       population: '20.27M',
       // https://commons.wikimedia.org/wiki/File:Flag_of_Florida.svg
       flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg',
     },
     {
+      id: 4,
       name: 'Texas',
       population: '27.47M',
       // https://commons.wikimedia.org/wiki/File:Flag_of_Texas.svg
@@ -67,17 +72,18 @@ export class AutocompleteOverviewExample {
   constructor() {
     this.filteredStates = this.stateCtrl.valueChanges.pipe(
       startWith(''),
-      map(state => (state ? this._filterStates(state) : this.states.slice())),
+      map((state) => (state ? this._filterStates(state) : this.states.slice()))
     );
   }
 
   private _filterStates(value: string): State[] {
     const filterValue = value.toLowerCase();
 
-    return this.states.filter(state => state.name.toLowerCase().includes(filterValue));
+    return this.states.filter((state) =>
+      state.name.toLowerCase().includes(filterValue)
+    );
   }
 }
-
 
 /**  Copyright 2023 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
